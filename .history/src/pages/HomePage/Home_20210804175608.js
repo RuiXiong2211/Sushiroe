72a0c93b535b4dee0ca50f02609797498336a613
RuiXiong2211 <ruixiong8@hotmail.com>
2021-08-04 17:57:01 +0800

    add scroll-snapping.

---
+++ .history/src/pages/HomePage/Home_20210804175608.js
@@ -0,0 +1,20 @@
+import { React, useRef, useEffect } from "react";
+import { InfoSection } from "../../components";
+import { homeObjOne, homeObjTwo, homeObjThree } from "../../data/home/Data";
+import useScrollSnap from "react-use-scroll-snap";
+
+/* to utilise map fnction */
+
+const Home = () => {
+  const scrollRef = useRef(null);
+  useScrollSnap({ ref: scrollRef, duration: 50, delay: 10 });
+  return (
+    <div ref={scrollRef}>
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
+    </div>
+  );
+};
+
+export default Home;
